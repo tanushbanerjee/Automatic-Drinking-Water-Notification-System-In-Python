286f74d23587f52827b43320fed74eb4f3bd4980
tanushbanerjee <samihanbanerjee026@gmail.com>
2020-08-17 13:45:53 +0530

    Added the command for running in background

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import time
 from plyer import notification
 
 if __name__ == "__main__":
-    notification.notify(
-        title = "***Please drink water now",
-        message = "The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
-        timeout = 10,
-        app_icon = "D:\\Drinking Water Notification System\\icon.ico"
-    )
+    while True:
+        notification.notify(
+            title = "***Please drink water now",
+            message = "The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
+            timeout = 10,
+            app_icon = "D:\\Drinking Water Notification System\\icon.ico"
+        )
+        time.sleep(60 * 60)
