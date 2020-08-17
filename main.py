02ec4735a4914c9ac68b04ffdc245d3d9861dba8
tanushbanerjee <samihanbanerjee026@gmail.com>
2020-08-17 13:41:00 +0530

    This is the drinking water notification system

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+    notification.notify(
+        title = "***Please drink water now",
+        message = "The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
+        timeout = 10,
+        app_icon = "D:\\Drinking Water Notification System\\icon.ico"
+    )
