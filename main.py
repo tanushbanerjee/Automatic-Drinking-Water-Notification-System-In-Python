e0f2b6761ceb8b8b0f4704e3335355bef639822d
Tanush Banerjee <69428961+tanushbanerjee@users.noreply.github.com>
2020-08-25 12:29:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
             title = "***Please drink water now",
             message = "The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
             timeout = 10,
-            app_icon = "D:\\Drinking Water Notification System\\icon.ico"
+            app_icon = "xxxxxxxxxxxxxxxxxxxxxxx"  # Address to icon
         )
         time.sleep(60 * 60)
